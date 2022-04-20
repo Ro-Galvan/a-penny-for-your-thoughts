63b9b8985ff13ba0daeb8b1ba1f0f75863939f65
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2022-04-20 16:49:46 -0400

    added controller directory to better organize the routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,30 +1,15 @@
 const router = require('express').Router();
-// const { Thought } = require('../models'); //is this needed here??
 
-app.post('/thoughts', (req, res) => {
-  // Use db connection to add a document
-  db.collection('thoughtCollection').insertOne(
-    { name: req.body.name, breed: req.body.breed },
-    (err, results) => {
-      if (err) throw err;
-      res.json(results);
-    }
-  );
-});
+const {
+  // getComments,
+  // getSingleComment,
+  // createComment,
+} = require('../../controllers/thoughtController');
 
+// /api/comments
+router.route('/').get(getComments).post(createComment);
 
-
-// Finds all departments
-app.get('/thoughts', (req, res) => {
-  // Using model in route to find all documents that are instances of that model
-  Thought.find({}, (err, result) => {
-    if (result) {
-      res.status(200).json(result);
-    } else {
-      console.log('Uh Oh, Internal Server Error', err);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
-});
+// /api/comments/:commentId
+router.route('/:commentId').get(getSingleComment);
 
 module.exports = router;
