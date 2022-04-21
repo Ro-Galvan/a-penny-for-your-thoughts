20470753a761b807b999592c4cbfaa49e6ba4575
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2022-04-21 16:53:04 -0400

    added createThought to ThoughtRoutes and app is now functioning and listening

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -3,11 +3,11 @@ const router = require('express').Router();
 const {
   getThoughts,
   getSingleThoughts,
-  // createComment,
+  createThought,
 } = require('../../controllers/thoughtController');
 
 // /api/thoughts
-router.route('/').get(getThoughts).post(createComment);
+router.route('/').get(getThoughts).post(createThought);
 
 // /api/thoughts/:thoughtId
 router.route('/:thoughtId').get(getSingleThoughts);
