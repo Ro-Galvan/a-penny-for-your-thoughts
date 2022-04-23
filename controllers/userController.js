027a1ac3bcc5f5dd24de7a38c89320cb81a67df5
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2022-04-23 18:32:48 -0400

    added update user routed and added it and deleteUser to the UserRoutes-tested and both working correctly

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,24 +5,24 @@ const { User, Thought } = require('../models');
 module.exports = {
   getUsers(req, res) {
     User.find()
-    // .populate('Thought') //maybe not needed--will replace id with object for thought 
+      // .populate('Thought') //maybe not needed--will replace id with object for thought 
       .then((users) => res.json(users))
       .catch((err) => res.status(500).json(err));
   },
   // GET a single user by its _id and populated thought and friend data
   getSingleUser(req, res) {
     User.findOne({ _id: req.params.userId })
-    .select('-__v')
-    .populate('thoughts') //activity 23- from User.js line 18
-    .then((user) =>
-    !user
-    ? res.status(404).json({ message: 'No user with that ID' })
-    : res.json(user)
-    )
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err)
-    });
+      .select('-__v')
+      .populate('thoughts') //activity 23- from User.js line 18
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json(user)
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err)
+      });
   },
   // POST/create a new user
   createUser(req, res) {
@@ -32,6 +32,19 @@ module.exports = {
   },
 
   // PUT to update a user by its _id
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with this id!' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 
   // DELETE to remove user by its _id
   deleteUser(req, res) {
@@ -45,16 +58,27 @@ module.exports = {
       .catch((err) => res.status(500).json(err));
   },
 
-//   /api/users/:userId/friends/:friendId
-// POST to add a new friend to a user's friend list
-addFriend(req, res) {
-  // find and update 
-  User.findByIdAndUpdate(req.params.userId, 
-    // {push user id into friend array})
-    .then((dbUserData) => res.json(dbUserData))
-    .catch((err) => res.status(500).json(err));
-},
+  //   /api/users/:userId/friends/:friendId
+  // POST to add a new friend to a user's friend list
+  addFriend(req, res) {
+    console.log('You are adding a new friend');
+    console.log(req.body);
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.body } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res
+            .status(404)
+            .json({ message: 'No user found with that ID :(' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
 
-// DELETE to remove a friend from a user's friend list
+  },
+
+  // DELETE to remove a friend from a user's friend list
 
 };
